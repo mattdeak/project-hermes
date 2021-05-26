baf080f07456cf80cff5ac9043bf3bf002003c3f
Matthew Deakos <mwoolnerdeakos@gmail.com>
2021-05-26 14:41:50 -0300

    more kraken dev

--- hermes/exchanges/kraken.py
+++ hermes/exchanges/kraken.py
@@ -1,3 +1,7 @@
+import logging
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)s  %(name)s| %(message)s")
+
 import websockets
 import traceback
 import asyncio
@@ -5,17 +9,19 @@ import json
 from hermes.orderbook.orderbook import MultiOrderBook
 from dataclasses import dataclass
 import logging
-
-logging.basicConfig(level=logging.INFO)
+from hermes.strategies.arbitrage.triangle import TriangleBSS
 
 DEMO_URL = "wss://ws.kraken.com"
 
+DEBUG = False
+
+TICKERS = ["XBT/CAD", "ETH/CAD", "XRP/CAD", "ETH/XBT", "XRP/XBT", "XRP/ETH"]
+TRIANGLE1 = ["ETH/CAD", "ETH/XBT", "XBT/CAD"]
+TRIANGLE2 = ["XRP/CAD", "XRP/XBT", "XBT/CAD"]
+TRIANGLE3 = ["XRP/CAD", "XRP/ETH", "ETH/CAD"]
+
 event_subscription = json.dumps(
-    {
-        "event": "subscribe",
-        "pair": ["XBT/CAD", "ETH/XBT", "ETH/CAD"],
-        "subscription": {"name": "book"},
-    }
+    {"event": "subscribe", "pair": TICKERS, "subscription": {"name": "book"},}
 )
 
 
@@ -27,12 +33,28 @@ class KrakenL2Update:
     Side: int
 
 
-DEPTH=10
-async def print_orderbook_at_interval(orderbook, interval=5):
+DEPTH = 10
+
+
+def get_kraken_message_type(payload):
+    if isinstance(payload, list):
+        # L2 Update or Snapshot
+        if "book" in payload[-2]:
+            if True:  # TODO: detect snapshot vs l2 update
+                return "snapshot"
+            else:
+                return "l2update"
+    elif isinstance(payload, list):
+        if payload.get("event"):
+            return payload["event"]
+
+
+async def print_orderbook_at_interval(orderbook, interval=1200):
+    await asyncio.sleep(10)  # wait for a bit to initialize
     while True:
-        await asyncio.sleep(interval)
-        print('Printing Orderbook')
+        print("Printing Orderbook")
         orderbook.print_orderbook()
+        await asyncio.sleep(interval)
 
 
 async def test(orderbook):
@@ -47,11 +69,13 @@ async def test(orderbook):
         await session.recv()
         await session.recv()
 
-        snapshot_received = {"XBT/CAD": False, "ETH/XBT": False, "ETH/CAD": False}
+        snapshot_received = {ticker: False for ticker in TICKERS}
+        triangle1 = TriangleBSS(orderbook, instrument_ids=TRIANGLE1, fee=0.0022)
+        triangle2 = TriangleBSS(orderbook, instrument_ids=TRIANGLE2, fee=0.0022)
+        triangle3 = TriangleBSS(orderbook, instrument_ids=TRIANGLE3, fee=0.0022)
 
         async for raw_message in session:
             try:
-
                 message = json.loads(raw_message)
 
                 if not isinstance(message, list):
@@ -63,8 +87,29 @@ async def test(orderbook):
                 else:
                     await orderbook.update(message)
 
+                for triangle in [triangle1, triangle2, triangle3]:
+                    if DEBUG:
+                        print(f'Forward: {triangle.forward()}')
+                        print(f'Backward: {triangle.backward()}')
+                    forward_val = triangle.forward_net(100)
+                    backward_val = triangle.backward_net(100)
+
+
+                    if forward_val > 0.1:
+                        print(triangle.instrument_ids)
+                        print("Forward Value > 10 cents detected")
+                        print("--")
+                        print(f"Val: {forward_val}")
+                        orderbook.print_orderbook()
+
+                    if backward_val > 0.1:
+                        print(triangle.instrument_ids)
+                        print("Backward Value > 10 cents detected")
+                        print("--")
+                        print(f"Val: {backward_val}")
+                        orderbook.print_orderbook()
             except Exception as e:
-                traceback.print_exc()
+                print(e)
 
     except Exception as e:
         await session.close()
@@ -87,18 +132,23 @@ class KrakenOrderBook(MultiOrderBook):
             await self.handle_update(l2update)
 
     async def update(self, payload):
-        try:
+        if len(payload) == 4:
             _id, book, _, instrument = payload
-        except Exception as e:
-            print(e)
-            print(payload)
-            print(len(payload))
-            return
-        for ask in book.get("a", []):
+            ask_book = book.get("a", [])
+            bid_book = book.get("b", [])
+
+        elif len(payload) == 5:
+            _id, ask_book, bid_book, _, instrument = payload
+            ask_book = ask_book["a"]
+            bid_book = bid_book["b"]
+        else:
+            raise ValueError(f"What the fuck is this: {payload}")
+
+        for ask in ask_book:
             l2update = KrakenL2Update(instrument, *ask[:2], Side=1)
             await self.handle_update(l2update)
 
-        for bid in book.get("b", []):
+        for bid in bid_book:
             l2update = KrakenL2Update(instrument, *bid[:2], Side=0)
             await self.handle_update(l2update)
 
@@ -114,14 +164,15 @@ class KrakenOrderBook(MultiOrderBook):
             try:
                 book_to_update.pop(price)
             except KeyError:
-                pass
+                print("why is there a key error")
         else:
             book_to_update[price] = quantity
+
 
 async def gather_all_tasks(orderbook):
     await asyncio.gather(test(orderbook), print_orderbook_at_interval(orderbook))
 
 
 if __name__ == "__main__":
-    orderbook = KrakenOrderBook(instrument_keys=["XBT/CAD", "ETH/XBT", "ETH/CAD"], depth=DEPTH, use_depth_limiter=False)
+    orderbook = KrakenOrderBook(instrument_keys=TICKERS, depth=DEPTH)
     asyncio.run(gather_all_tasks(orderbook))
